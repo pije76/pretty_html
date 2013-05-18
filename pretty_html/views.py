b4d1857828edbfe8428726db50aba8a01dec0560
Fanani M. Ihsan <contact@fanani.net>
2013-05-19 00:28:32 +0700

    add result page

--- pretty_html/views.py
+++ pretty_html/views.py
@@ -1,12 +1,19 @@
 from pretty_html import app
-from flask import render_template
+from flask import render_template, redirect
 
 from .forms import HtmlPrettify
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
     form = HtmlPrettify()
-    if form.validate_on_submit():
-        print "on post"
     return render_template("index.html", form=form)
+
+
+@app.route("/result", methods=["GET", "POST"])
+def result():
+    form = HtmlPrettify()
+    if form.validate_on_submit:
+        return render_template("result.html")
+
+    return redirect('/')
