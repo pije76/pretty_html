2ae8e721e63ec957d9336b61370c17b80592927c
Fanani M. Ihsan <contact@fanani.net>
2013-05-19 00:22:12 +0700

    fix form

--- pretty_html/forms.py
+++ pretty_html/forms.py
@@ -1,6 +1,7 @@
-from flask.ext.wtf import Form, TextAreaField, TextField, Required
+from flask.ext.wtf import Form, TextAreaField, Required
+from flask.ext.wtf.html5 import IntegerField
 
 
 class HtmlPrettify(Form):
     html = TextAreaField('HTML', validators=[Required()])
-    indentation = TextField('indentation', validators=[Required()], default=4)
+    indentation = IntegerField('indentation', validators=[Required()], default=4)
--- pretty_html/views.py
+++ pretty_html/views.py
@@ -4,7 +4,9 @@ from flask import render_template
 from .forms import HtmlPrettify
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     form = HtmlPrettify()
+    if form.validate_on_submit():
+        print "on post"
     return render_template("index.html", form=form)
